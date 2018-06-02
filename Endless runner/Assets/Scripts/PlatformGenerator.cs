d9f82590be9ab3e07508e87d70508180f099b6f3
Balraj <kadaikal@usc.edu>
2018-06-01 18:32:48 -0700

    First push - Movement, jumping, camera follow

---
+++ Endless runner/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Endless runner/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D myRigidbody;
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+
+	private Collider2D myCollider;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+		grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+			if (grounded) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+			}
+		}
+	}
+}
