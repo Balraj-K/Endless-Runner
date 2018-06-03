9e961dad3849cc48c22dd44697a853ceca3746f1
Balraj <kadaikal@usc.edu>
2018-06-03 04:25:04 -0700

    Implemented basic object pooler to generate platforms

--- Endless runner/Assets/Scripts/PlatformGenerator.cs
+++ Endless runner/Assets/Scripts/PlatformGenerator.cs
@@ -13,9 +13,12 @@ public class PlatformGenerator : MonoBehaviour {
 	public float distanceBetweenMin;
 	public float distanceBetweenMax;
 
+	public ObjectPooler theObjectPool;
+
 	// Use this for initialization
 	void Start () {
 		platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+		//Initially setting a platform width variable to the actual width of the platform. To use in calculation below
 
 	}
 	
@@ -24,10 +27,19 @@ public class PlatformGenerator : MonoBehaviour {
 		if (transform.position.x < generationPoint.position.x) {
 
 			distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+			//setting random distance between platforms
 
 			transform.position = new Vector3 (transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
-			Instantiate(thePlatform, transform.position, transform.rotation);
+			//Instantiate(thePlatform, transform.position, transform.rotation);
+			//To implement object pool we don't instantiate objects on the go. Instead we create a pool of 
+			// objects which we toggle between active and inactive. In PlatformDestroyer we make them inactive
+		
+			GameObject newPlatform = theObjectPool.GetPooledObject();
+			newPlatform.transform.position = transform.position;
+			newPlatform.transform.rotation = transform.rotation;
+			newPlatform.SetActive(true);
 		}
+
 	}
 }
